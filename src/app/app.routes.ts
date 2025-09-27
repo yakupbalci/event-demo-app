c23343ccf05cb55a37771082a406db455155b27e
Yakup Balci <yakup.balci599@gmail.com>
2025-09-27 12:46:55 +0200

    Set up project environment to match demo requirements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/event-overview',
+    pathMatch: 'full',
+  },
+  {
+    path: 'event-overview',
+    title: 'Events',
+    loadComponent: () =>
+      import('./pages/event-overview/event-overview').then((m) => m.EventOverview),
+  },
+];
